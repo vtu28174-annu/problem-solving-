33bd209c1f6a2fec91cd0f62cb1bfff678ba3f74
vtu28174-annu <vtu28174@veltech.edu.in>
2026-08-02 11:56:51 +0530

    Add files via upload

---
+++ Array of size N.java
@@ -0,0 +1,13 @@
+public class AllPairs {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+
+        System.out.println("All possible pairs are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.println("(" + arr[i] + ", " + arr[j] + ")");
+            }
+        }
+    }
+}
